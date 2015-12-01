14e82649be682d9882a84a7df0231029c7ca40d7
Claudio Capobianco <wbigger@gmail.com>
2015-12-01 11:05:50 +0100

    add check on minimun required matlab version

--- src/matlab/startHere.m
+++ src/matlab/startHere.m
@@ -1,4 +1,15 @@
 % Launch this script to get started
 
+%%
+disp('Checking MATLAB version...');
+if verLessThan('matlab','8.1')
+    % -- MATLAB R2012b and earlier --
+    error('matlab:version',['You need MATLAB R2013a or later to run ASTEF. Your version is ' version('-release')]);
+else
+    % -- MATLAB R2013a and later here --
+    disp('OK');
+end
+
+%%
 projectPaths;
 replay;
