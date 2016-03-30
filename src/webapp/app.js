9b105ce0db35d9d61433a1f661a2877794609f54
Francesco Malatesta <hellofrancesco@gmail.com>
2016-03-30 09:03:19 +0200

    First webapp proposal

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -1,12 +1,110 @@
-function readNewFile(event){
-    var f = event.target.files[0];
+var tValues = [];
+var xValues = [];
+var yValues = [];
 
-    if (!f) {
+function populateValues(valuesFileLines) {
+    var line;
+    for(var c in valuesFileLines) {
+        if(c != valuesFileLines.length - 1) {
+            line = valuesFileLines[c].split(' ');
+
+            tValues.push(line[0]);
+            xValues.push(line[1]);
+            yValues.push(line[2]);
+        }
+    }
+}
+
+function buildChart() {
+    resetCanvas();
+
+    var data = {
+        labels: tValues,
+        datasets: [
+            {
+                label: "X Coordinate",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: xValues
+            },
+            {
+                label: "Y Coordinate",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: yValues
+            }
+        ]
+    };
+
+    var ctx = document.getElementById("chart").getContext("2d");
+    var myLineChart = new Chart(ctx).Line(data);
+}
+
+// Note: in this validation function the final empty line is not contemplated.
+function isAValidFile(fileContentArray) {
+    if(/(\d{1,}) (\d{1,})/.exec(fileContentArray[0]) === null){
+        return false;
+    }
+
+    if(fileContentArray[1] != 'Timestamp Fix_x Fix_y') {
+        return false;
+    }
+
+    var restOfTheFile = fileContentArray.slice().splice(2);
+
+    for(var c in restOfTheFile) {
+        var currentLine = restOfTheFile[c];
+
+        if(c == restOfTheFile.length - 1) {
+            if(currentLine != '' && /(\d{1,}) (\d{1,}) (\d{1,})/.exec(restOfTheFile[c]) === null) {
+                return false;
+            }
+        } else {
+            if(/(\d{1,}) (\d{1,}) (\d{1,})/.exec(restOfTheFile[c]) === null) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function readNewFile(event){
+    tValues = [];
+    xValues = [];
+    yValues = [];
+
+    var file = event.target.files[0];
+
+    if (!file) {
         alert("Failed to load the file.");
-    } else if (!f.type.match('text.*')) {
-        alert(f.name + " is not a valid text file!");
+    } else if (!file.type.match('text.*')) {
+        alert(file.name + " is not a valid text file!");
     } else {
-        // TODO: create json data for chart, create it
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(event) {
+            var fileContentArray = fileReader.result.split("\n");
+
+            if(isAValidFile(fileContentArray)) {
+                var valuesFileLines = fileContentArray.slice().splice(2);
+
+                populateValues(valuesFileLines);
+                buildChart();
+            } else {
+                alert('The uploaded file does not respect the requested format.');
+            }
+        };
+
+        fileReader.readAsText(file);
     }
 }
 
@@ -17,6 +115,11 @@ function checkFileReaderSupport(){
         alert('The File APIs are not fully supported by your browser.');
         $('#upload').prop('disabled', true);
     }
+}
+
+function resetCanvas() {
+    $('#chart').remove();
+    $('#canvas-container').html('<canvas id="chart"></canvas>');
 }
 
 $(document).ready(function(){
