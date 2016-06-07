118925efb270c1e2b4a6d8844ef0973de0b0fdfd
Claudio Capobianco <c.capobianco@snapback.io>
2016-06-07 12:17:30 +0200

    added tests for input file format check

---
+++ src/matlab/astef/replay/test/test.m
@@ -0,0 +1,27 @@
+addpath('../');
+
+inputFilesRelPath = '../../../../../demo_data/test/';
+fname = 'source-ok.txt';
+
+importFixation([inputFilesRelPath fname]);
+
+fnames = {'source-err-1.txt'...
+    'source-err-1.1.txt'...
+    'source-err-1.2.txt'...
+    'source-err-2.txt'...
+    'source-err-3.txt'...
+    'source-err-4.txt'...
+    'source-err-5.txt'};
+
+for f = fnames
+    fullname = [inputFilesRelPath f{1}];
+    if exist(fullname,'file')
+        try
+            importFixation(fullname);
+            disp([f{1} ': test FAILED']);
+        catch
+        end
+    else
+        disp([f{1} ': test file does not exists']);
+    end
+end
